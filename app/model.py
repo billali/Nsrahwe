77e1169286e92b279d4696c3ac107c06cfa1e29c
kwakyesamuelosei <kwakyesamuelosei@gmail.com>
2018-07-17 14:24:41 +0000

    After forcible reformat

--- app/model.py
+++ app/model.py
@@ -10,87 +10,86 @@ class Region(object):
     
 
     def getTouriteSiteByRegion(self,data):
-	    conn = ""
-	    out = []
-	    try:
-	        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-	        dbname = url.path[1:]
-	        user = url.username
-	        password = url.password
-	        host = url.hostname
-	        port = url.port
-	        conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-	    except:
-	        out = {"err": "Unable to connect to the database"}
-	    try:
-			cur = conn.cursor()
-			cur.execute("""SELECT * from tbl_tourist_sites where region_shortname = '{0}'""".format(data['region_shortname']))
-			rows = cur.fetchall()
-			
-			data = []
-			for row in rows:
-				data.append({ "id": row[0], "tourist_site_name": row[1], "tourist_site_image": row[2], "tourist_alt_name": row[3], "region_shortname": row[4], "tourist_site_description": row[5] })
-				
-			if data == []:
-				out = {'code': '01', 'msg': 'Failed to retrieve data', 'data': [] }
-			else:
-				out = { 'code': '00', 'msg': 'Data Retrieved Successfully', 'data': data }
-			""	
-	    except:
-	        out = {"err": "General SQL Error"}
-	        
-		return json.dumps(out)
+        conn = ""
+        out = []
+        try:
+            url = urlparse.urlparse(os.environ['DATABASE_URL'])
+            dbname = url.path[1:]
+            user = url.username
+            password = url.password
+            host = url.hostname
+            port = url.port
+            conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        except:
+            out = {"err": "Unable to connect to the database"}
+        try:
+         cur = conn.cursor()
+         cur.execute("""SELECT * from tbl_tourist_sites where region_shortname = '{0}'""".format(data['region_shortname']))
+         rows = cur.fetchall()
+         
+         data = []
+         for row in rows:
+          data.append({ "id": row[0], "tourist_site_name": row[1], "tourist_site_image": row[2], "tourist_alt_name": row[3], "region_shortname": row[4], "tourist_site_description": row[5] })
+          
+         if data == []:
+          out = {'code': '01', 'msg': 'Failed to retrieve data', 'data': [] }
+         else:
+          out = { 'code': '00', 'msg': 'Data Retrieved Successfully', 'data': data }
+         ""   
+        except:
+            out = {"err": "General SQL Error"}
+       return json.dumps(out)
 
-	def getTouriteSiteById(self,data):
-		conn = ""
-		out = []
-		try:
-			url = urlparse.urlparse(os.environ['DATABASE_URL'])
-	        dbname = url.path[1:]
-	        user = url.username
-	        password = url.password
-	        host = url.hostname
-	        port = url.port
-	        conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-	    except:
-	        out = {"err": "Unable to connect to the database"}
-	    try:
-	        cur = conn.cursor()
-	        cur.execute("""SELECT * from tbl_tourist_sites where id = '{0}'""".format(data['id']))
-	        rows = cur.fetchall()
-	        data = []
-	        for row in rows:
-	            data.append(
-	            	{
+    def getTouriteSiteById(self,data):
+       conn = ""
+       out = []
+       try:
+         url = urlparse.urlparse(os.environ['DATABASE_URL'])
+            dbname = url.path[1:]
+            user = url.username
+            password = url.password
+            host = url.hostname
+            port = url.port
+            conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        except:
+            out = {"err": "Unable to connect to the database"}
+        try:
+            cur = conn.cursor()
+            cur.execute("""SELECT * from tbl_tourist_sites where id = '{0}'""".format(data['id']))
+            rows = cur.fetchall()
+            data = []
+            for row in rows:
+                data.append(
+                   {
 
-	            	"id": row[0], 
+                   "id": row[0], 
                     "tourist_site_name": row[1],
                     "tourist_site_image": row[2], 
                     "tourist_alt_name": row[3],
                     "region_shortname": row[4], 
                     "tourist_site_description": row[5],
-	            }
+                }
 
-	            )
-	        if data!=[]:
+                )
+            if data!=[]:
 
-	        	data = {
-    			'code':'00',
-    			'msg':'Data Retrieved Successfully',
-    			'data':data
-    		}
-    		return data
+               data = {
+             'code':'00',
+             'msg':'Data Retrieved Successfully',
+             'data':data
+           }
+           return data
 
-	        else:
+            else:
 
-	        	data = {
-    			'code':'01',
-    			'msg':'Failed to retrieve data',
-    			'data':[]
-    		}
-    		return data
+               data = {
+             'code':'01',
+             'msg':'Failed to retrieve data',
+             'data':[]
+           }
+           return data
 
-	    except:
-	        out = {"err": "General SQL Error"}
-	    return json.dumps(out)
+        except:
+            out = {"err": "General SQL Error"}
+        return json.dumps(out)
   
