463d78889d95a01b8911e438fdd9008887ba6b56
sammy <kwakyesamuelosei@gmail.com>
2018-07-17 12:49:34 +0000

    Adding changes on model file

--- run.py
+++ run.py
@@ -4,8 +4,10 @@ The File contains the script that starts the application
 
 # Run a application server.
 from app.application import app
-from app.config import APP_PORT, APP_HOST, DEBUG
+import urllib.parse as urlparse
+import os
 
 #import app
 if __name__ == '__main__':
-    app.run(host=APP_HOST, port=APP_PORT, debug=DEBUG)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
