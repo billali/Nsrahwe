cf0239a6193314ade40db3af924f7e73fb570a2e
sammy <kwakyesamuelosei@gmail.com>
2018-07-19 07:44:29 +0000

    Connecting to Flask SQLALCHEMY DB

--- app/model.py
+++ app/model.py
@@ -5,121 +5,10 @@ import urllib.parse as urlparse
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-# class Region(object):
-#     """docstring for UserFunctions"""
-#     def __init__(self):
-#         super(Region, self).__init__()
-
-#     def getTouristSiteWeather(self,city):
-#         print(city)
-#         if city!="":
-#             url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric"+ "&APPID=e2cd7d4ec205a87e9fedc5b591cedd72"
-#             result = requests.get(url)
-#             result = json.loads(result.text)
-#             weather_data = {
-#                 'temp': result['main']['temp'],
-#                 'humidity': result['main']['humidity']
-#             }
-            
-
-#             return weather_data
-    
-
-#     def getTouriteSiteByRegion(self,data):
-#         conn = ""
-#         out = []
-#         try:
-#             url = urlparse.urlparse(os.environ['DATABASE_URL'])
-#             dbname = url.path[1:]
-#             user = url.username
-#             password = url.password
-#             host = url.hostname
-#             port = url.port
-#             conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-#         except:
-#             out = {"err": "Unable to connect to the database"}
-#         try:
-#             cur = conn.cursor()
-#             cur.execute("""SELECT * from tbl_tourist_site where region_shortname = '{0}'""".format(data['region_shortname']))
-#             rows = cur.fetchall()
-         
-#             data = []
-#             for row in rows:
-#                 data.append({ "id": row[0], "tourist_site_name": row[1], "tourist_site_image": row[2], "tourist_alt_name": row[3], "region_shortname": row[4], "tourist_site_description": row[5] })
-          
-#             if data == []:
-#                 out = {'code': '01', 'msg': 'Failed to retrieve data', 'data': [] }
-#             else:
-#                 out = { 'code': '00', 'msg': 'Data Retrieved Successfully', 'data': data }
-#         except:
-#             out = {"err": "General SQL Error"}
-#         return json.dumps(out)
-
-#     def getTouriteSiteById(self,data):
-#         conn = ""
-#         out = []
-#         try:
-#             url = urlparse.urlparse(os.environ['DATABASE_URL'])
-#             dbname = url.path[1:]
-#             user = url.username
-#             password = url.password
-#             host = url.hostname
-#             port = url.port
-#             conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
-#         except:
-#             out = {"err": "Unable to connect to the database"}
-#         try:
-#             cur = conn.cursor()
-#             cur.execute("""SELECT * from tbl_tourist_site where id = '{0}'""".format(data['id']))
-#             rows = cur.fetchall()
-#             data = []
-#             for row in rows:
-#                 data.append({
-#                     "id": row[0], 
-#                     "tourist_site_name": row[1],
-#                     "tourist_site_image": row[2], 
-#                     "tourist_alt_name": row[3],
-#                     "region_shortname": row[4], 
-#                     "tourist_site_description": row[5] })
-            
-#             if data != []:
-#                 # weather_data = weather.getTouristSiteWeather(data['city'])
-#                 out = {
-#                     'code':'00',
-#                     'msg':'Data Retrieved Successfully',
-#                     #'weather':weather_data,
-#                     'data':data }
-#             else:
-#                 out = {
-#                     'code':'01',
-#                     'msg':'Failed to retrieve data',
-#                     'data':[] }
-#         except:
-#             out = {"err": "General SQL Error"}
-#         return json.dumps(out)
-
-
-
-
-db = SQLAlchemy()
-
-class Region(db.Model):
-    __tablename__ = "tourist_site"
-    id = db.Column(db.Integer, primary_key=True)
-    tourist_site_name = db.Column(db.String(120), unique=True)
-    tourist_site_image = db.Column(db.String(120), unique=True)
-    tourist_alt_name = db.Column(db.String(120), unique=True)
-    region_shortname = db.Column(db.String(120), unique=True)
-    tourist_site_description = db.Column(db.String(120), unique=True)
-
-    def __init__(self, tourist_site_name='',tourist_site_image='',tourist_alt_name='',region_shortname='',tourist_site_description=''):
-        self.tourist_site_name = tourist_site_name,
-        self.tourist_site_image = tourist_site_image
-        self.tourist_alt_name = tourist_alt_name
-        self.region_shortname = region_shortname
-        self.tourist_site_description = tourist_site_description
-
-
+class Region(object):
+    """docstring for UserFunctions"""
+    def __init__(self):
+        super(Region, self).__init__()
 
     def getTouristSiteWeather(self,city):
         print(city)
@@ -137,30 +26,51 @@ class Region(db.Model):
     
 
     def getTouriteSiteByRegion(self,data):
+        conn = ""
+        out = []
         
         try:
-            
-            result = Region.query.filter_by(region_shortname = '{0}'.format(data['region_shortname'])).all()
-            db.session.commit()
-            print("Printing data from db")
-            print(result)
-            # data = []
-            # for row in rows:
-            #     data.append({ "id": row[0], "tourist_site_name": row[1], "tourist_site_image": row[2], "tourist_alt_name": row[3], "region_shortname": row[4], "tourist_site_description": row[5] })
+            url = urlparse.urlparse(os.environ['DATABASE_URL'])
+            dbname = url.path[1:]
+            user = url.username
+            password = url.password
+            host = url.hostname
+            port = url.port
+            conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        except Exception as e:
+            out = {"err1": str(e)}
+        try:
+            cur = conn.cursor()
+            cur.execute("""SELECT * from tbl_tourist_site where region_shortname = '{0}'""".format(data['region_shortname']))
+            rows = cur.fetchall()
+         
+            data = []
+            for row in rows:
+                data.append({ "id": row[0], "tourist_site_name": row[1], "tourist_site_image": row[2], "tourist_alt_name": row[3], "region_shortname": row[4], "tourist_site_description": row[5] })
           
-            if result == []:
+            if data == []:
                 out = {'code': '01', 'msg': 'Failed to retrieve data', 'data': [] }
             else:
-                out = { 'code': '00', 'msg': 'Data Retrieved Successfully', 'data': result }
-        except Exception as e:
-            out = {"err": str(e)}
+                out = { 'code': '00', 'msg': 'Data Retrieved Successfully', 'data': data }
+        except:
+            out = {"err2": str(e)}
         return json.dumps(out)
 
     def getTouriteSiteById(self,data):
-        
-        
+        conn = ""
+        out = []
         try:
-            
+            url = urlparse.urlparse(os.environ['DATABASE_URL'])
+            dbname = url.path[1:]
+            user = url.username
+            password = url.password
+            host = url.hostname
+            port = url.port
+            conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        except:
+            out = {"err": "Unable to connect to the database"}
+        try:
+            cur = conn.cursor()
             cur.execute("""SELECT * from tbl_tourist_site where id = '{0}'""".format(data['id']))
             rows = cur.fetchall()
             data = []
@@ -188,6 +98,98 @@ class Region(db.Model):
         except:
             out = {"err": "General SQL Error"}
         return json.dumps(out)
+
+
+
+
+# db = SQLAlchemy()
+
+# class Region(db.Model):
+#     __tablename__ = "tourist_site"
+#     id = db.Column(db.Integer, primary_key=True)
+#     tourist_site_name = db.Column(db.String(120), unique=True)
+#     tourist_site_image = db.Column(db.String(120), unique=True)
+#     tourist_alt_name = db.Column(db.String(120), unique=True)
+#     region_shortname = db.Column(db.String(120), unique=True)
+#     tourist_site_description = db.Column(db.String(120), unique=True)
+
+#     def __init__(self, tourist_site_name='',tourist_site_image='',tourist_alt_name='',region_shortname='',tourist_site_description=''):
+#         self.tourist_site_name = tourist_site_name,
+#         self.tourist_site_image = tourist_site_image
+#         self.tourist_alt_name = tourist_alt_name
+#         self.region_shortname = region_shortname
+#         self.tourist_site_description = tourist_site_description
+
+
+
+#     def getTouristSiteWeather(self,city):
+#         print(city)
+#         if city!="":
+#             url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric"+ "&APPID=e2cd7d4ec205a87e9fedc5b591cedd72"
+#             result = requests.get(url)
+#             result = json.loads(result.text)
+#             weather_data = {
+#                 'temp': result['main']['temp'],
+#                 'humidity': result['main']['humidity']
+#             }
+            
+
+#             return weather_data
+    
+
+#     def getTouriteSiteByRegion(self,data):
+        
+#         try:
+            
+#             result = Region.query.filter_by(region_shortname = '{0}'.format(data['region_shortname'])).all()
+#             result = json.dumps(result)
+#             db.session.commit()
+#             print("Printing data from db")
+#             print(result)
+#             # data = []
+#             # for row in rows:
+#             #     data.append({ "id": row[0], "tourist_site_name": row[1], "tourist_site_image": row[2], "tourist_alt_name": row[3], "region_shortname": row[4], "tourist_site_description": row[5] })
+          
+#             if result == []:
+#                 out = {'code': '01', 'msg': 'Failed to retrieve data', 'data': [] }
+#             else:
+#                 out = { 'code': '00', 'msg': 'Data Retrieved Successfully', 'data': result }
+#         except Exception as e:
+#             out = {"err": str(e)}
+#         return json.dumps(out)
+
+#     def getTouriteSiteById(self,data):
+        
+        
+#         try:
+            
+#             cur.execute("""SELECT * from tbl_tourist_site where id = '{0}'""".format(data['id']))
+#             rows = cur.fetchall()
+#             data = []
+#             for row in rows:
+#                 data.append({
+#                     "id": row[0], 
+#                     "tourist_site_name": row[1],
+#                     "tourist_site_image": row[2], 
+#                     "tourist_alt_name": row[3],
+#                     "region_shortname": row[4], 
+#                     "tourist_site_description": row[5] })
+            
+#             if data != []:
+#                 # weather_data = weather.getTouristSiteWeather(data['city'])
+#                 out = {
+#                     'code':'00',
+#                     'msg':'Data Retrieved Successfully',
+#                     #'weather':weather_data,
+#                     'data':data }
+#             else:
+#                 out = {
+#                     'code':'01',
+#                     'msg':'Failed to retrieve data',
+#                     'data':[] }
+#         except:
+#             out = {"err": "General SQL Error"}
+#         return json.dumps(out)
 
 
 
